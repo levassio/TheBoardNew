b33e000377a7d0efa66578518ce3361664b16822
levassio <sergiy.yeskov@yahoo.com>
2014-12-16 02:44:20 +1100

    clean up 4

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-var http = require("http");
-var express = require("express");
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var expressSession = require("express-session");
-var app = express();
-var controllers = require("./controllers");
-var flash = require("connect-flash");
+var http = require("http")
+    , express = require("express")
+    , bodyParser = require('body-parser')
+    , cookieParser = require('cookie-parser')
+    , expressSession = require("express-session")
+    , app = express()
+    , controllers = require("./controllers")
+    , flash = require("connect-flash");
 
-// Setup the View Engine
 app.set("view engine", "vash");
 
-// Opt into Services
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -18,33 +16,14 @@ app.use(expressSession({secret: 'keyboard cat', resave: false, saveUninitialized
 
 app.use(flash());
 
-// set the public static resource folder
 app.use(express.static(__dirname + "/public"));
 
-// use authentication
 var auth = require("./auth");
 auth.init(app);
 
-// Map the routes
 controllers.init(app);
 
-app.get("/api/users", function (req, res) {
-    res.set("Content-Type", "application/json");
-    res.send({name: "Shawn", isValid: true, group: "Admin"});
-});
-
-app.get("/api/sql", function (req, res) {
-    var msnodesql = require("msnodesql");
-    var connString = "Driver={SQL Server Native Client 11.0};Server=.\\sqlexpress12;Database=Northwind;Trusted_Connection={Yes}";
-
-    msnodesql.query(connString, "SELECT * FROM Customers WHERE CustomerID = 'ALFKI'", function (err, results) {
-        // Error Handling
-        res.send(results);
-    });
-});
-
 var server = http.createServer(app);
-
 server.listen(3000);
 
 var updater = require("./updater");
--- updater/index.js
+++ updater/index.js
@@ -1,26 +1,23 @@
-﻿// updater/index.js
-(function (updater) {
+﻿(function (updater) {
 
-  var socketio = require("socket.io");
+    var socketio = require("socket.io");
 
-  updater.init = function (server) {
-    var io = socketio.listen(server);
+    updater.init = function (server) {
+        var io = socketio.listen(server);
 
-    io.sockets.on("connection", function (socket) {
+        io.sockets.on("connection", function (socket) {
 
-      console.log("socket was connected");
+            console.log("socket was connected");
 
-      //socket.emit("showThis", "this is from the server");
+            socket.on("join category", function (category) {
+                socket.join(category);
+            });
 
-      socket.on("join category", function (category) {
-        socket.join(category);
-      });
+            socket.on("newNote", function (data) {
+                socket.broadcast.to(data.category).emit("broadcast note", data.note);
+            });
 
-      socket.on("newNote", function (data) {
-        socket.broadcast.to(data.category).emit("broadcast note", data.note);
-      });
-
-    });
-  };
+        });
+    };
 
 })(module.exports);
