9d886b15109392dd209d695bacfcbbbbe584cae8
levassio <sergiy.yeskov@yahoo.com>
2014-12-16 02:29:59 +1100

    clean up 3

--- public/js/notesView.js
+++ public/js/notesView.js
@@ -8,24 +8,19 @@
                 $scope.notes = [];
                 $scope.newNote = createBlankNote();
 
-                // Get the category name
                 var urlParts = $window.location.pathname.split("/");
                 var categoryName = urlParts[urlParts.length - 1];
 
                 var notesUrl = "/api/notes/" + categoryName;
+
                 $http.get(notesUrl)
                     .then(function (result) {
-                        // success
                         $scope.notes = result.data;
                     }, function (err) {
-                        // Error
                         alert(err);
                     });
 
                 var socket = io.connect();
-                //socket.on("showThis", function (msg) {
-                //  alert(msg);
-                //});
 
                 socket.emit("join category", categoryName);
 
@@ -39,12 +34,10 @@
 
                     $http.post(notesUrl, $scope.newNote)
                         .then(function (result) {
-                            // success
                             $scope.notes.push(result.data);
                             $scope.newNote = createBlankNote();
                             socket.emit("newNote", {category: categoryName, note: result.data});
                         }, function (err) {
-                            // failure
                             // TODO
                         });
 
